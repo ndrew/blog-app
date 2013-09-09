f9008a2d11ca723c00c62586a9fce23d003f155f
Andrew Sernyak <ndrew.sernyak@gmail.com>
2013-09-10 01:34:23 +0300

    WIP: added plumbing for handling commands

--- blog.go
+++ blog.go
@@ -12,86 +12,120 @@ Static blog generator for http://sernyak.com
 */
 package main
 
-import(
-"flag"
-"fmt"
-"sort"
-"github.com/ndrew/blog"
+import (
+	"flag"
+	"fmt"
+	//blog "github.com/ndrew/blog"
+	"github.com/ndrew/blog"
+	"path/filepath"
+	"sort"
 )
 
-
 var COMMANDS = map[string]string{
-    "new": "<post-name> [<params>] - creates new post and opens editor", 
-    "edit": "<post-name>           - opens post in editor", 
-    "publish": "[<post-name>]      - renders markdown posts to html"}
-
+	"new":     "<post-name> [<params>] - creates new post and opens editor",
+	"edit":    "<post-name>           - opens post in editor",
+	"publish": "[<post-name>]      - renders markdown posts to html"}
 
 func printHeader() {
-    fmt.Println("╔════════════════════════════════════════╗")
-    fmt.Println("╟ ░░░░░░░░░░░ BLOG GENERATOR ░░░░░░░░░░░ ╢")
-    fmt.Println("╚════════════════════════════════════════╝")
-    fmt.Println("Usage: blog [--config <cfg-file>] [--help]")
-    fmt.Println("             <command> [<args>]")
-    fmt.Println("──────────────────────────────────────────")
+	fmt.Println("╔════════════════════════════════════════╗")
+	fmt.Println("╟ ░░░░░░░░░░░ BLOG GENERATOR ░░░░░░░░░░░ ╢")
+	fmt.Println("╚════════════════════════════════════════╝")
+	fmt.Println("Usage: blog [--config <cfg-file>] [--help]")
+	fmt.Println("             <command> [<args>]")
+	fmt.Println("──────────────────────────────────────────")
 }
-
 
 func listCommands(full_description bool) {
-    if (full_description) {
-        fmt.Println("Available commands:")
-    }
-    
-    var keys []string
-    for k := range COMMANDS {
-        keys = append(keys, k)
-    }
-    sort.Strings(keys)
-    for _, command := range keys {
-        if (full_description) {
-            fmt.Println(" ■", command, COMMANDS[command])
-        } else {
-            // TODO: params
-            fmt.Println(command)
-        }
-    }
+	if full_description {
+		fmt.Println("Available commands:")
+	}
+
+	var keys []string
+	for k := range COMMANDS {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	for _, command := range keys {
+		if full_description {
+			fmt.Println(" ■", command, COMMANDS[command])
+		} else {
+			// TODO: params
+			fmt.Println(command)
+		}
+	}
 }
 
+func newPost(args []string, engine *blog.Engine) {
+	// do something here
+	fmt.Println("new post!")
+	editPost(args, engine)
+}
 
-func NewPost(args []string) {
-    // do something here
-    fmt.Println("new post!")
+func editPost(args []string, engine *blog.Engine) {
+	// do something here
+	fmt.Println("edit post!")
+}
+
+func publishPosts(args []string, engine *blog.Engine) {
+	// do something here
+	fmt.Println("publish!")
 }
 
 func main() {
-    var configFile = ""
-    flag.StringVar(&configFile, "config", "default.xml", "help message for flagname")
-    flag.Parse()
 
-    blog.HelloFromLib() //ReadConfig(configFile)
-    //fmt.Println( cfg.Foo )
-    
-        
-    var action = flag.Arg(0)
-    if (action == "autocomplete") {
-        listCommands(false)
-        return 
-    } 
+	// TODO: loading from filesystem or --config
+	cfg := new(blog.Config)
+	cfg.BaseUrl = "http://localhost:666/blog/"
 
+	renderingStrategy := new(blog.RenderingStrategy)
 
-    var args = flag.Args()
-    var commandParams = []string{} 
-    if (len(args) > 1) {
-        commandParams = args[1:len(args)]    
-    }    
+	postsFactory := new(blog.FolderPostFactory)
+	postsFactory.PostsDir = "/Users/ndrw/Desktop/dev/site/blog/posts"
 
-    switch {
-        case action == "new": NewPost(commandParams)
-        case action == "edit": fmt.Println("edit")
-        case action == "publish": fmt.Println("publish")
+	posts := postsFactory.GetPosts()
 
-        default: {
-            printHeader()
-            listCommands(true)
-        } 
-    }
+	engine := blog.New(cfg, renderingStrategy, posts)
+	// engine.RunServer(".", "bla-bla")
+
+	var configFile = ""
+	flag.StringVar(&configFile, "config", "default.cfg", "help message for flagname")
+	flag.Parse()
+
+	var configFileAbs, err = filepath.Abs(configFile)
+
+	var metadata *blog.AppCfg = &blog.AppCfg{Foo: "tttt"}
+	if err != nil {
+		metadata = &blog.AppCfg{Foo: "xyz"}
+	} else {
+		metadata.ReadConfig(configFileAbs)
+	}
+
+	fmt.Println(metadata.Foo)
+
+	var action = flag.Arg(0)
+	if action == "autocomplete" {
+		listCommands(false)
+		return
+	}
+
+	var args = flag.Args()
+	var commandParams = []string{}
+	if len(args) > 1 {
+		commandParams = args[1:len(args)]
+	}
+
+	switch {
+	case action == "new":
+		newPost(commandParams, engine)
+	case action == "edit":
+		editPost(commandParams, engine)
+	case action == "publish":
+		publishPosts(commandParams, engine)
+
+	default:
+		{
+			printHeader()
+			listCommands(true)
+		}
+	}
 }
